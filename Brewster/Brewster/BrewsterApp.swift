fe8fa4819ac28d1db6108d850f726397e9839e72
JosiahM42 <64264040+JosiahM42@users.noreply.github.com>
2022-05-09 16:52:18 +0100

    Inital Creation

---
+++ Brewster/Brewster/BrewsterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewsterApp.swift
+//  Brewster
+//
+//  Created by Josiah Murray on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BrewsterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
