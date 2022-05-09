dd2f9e690a2acb72af6d5ecbdaaaf7905b172dfd
JosiahM42 <64264040+JosiahM42@users.noreply.github.com>
2022-05-09 18:27:20 +0100

    Added brewing method buttons

--- Brewster/Brewster/ContentView.swift
+++ Brewster/Brewster/ContentView.swift
@@ -9,16 +9,121 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack{
-            Text("Brewing Methods")
-                .font(.title2)
-                .padding(.top)
-                .multilineTextAlignment(.center)
-            VStack{
+        Text("Brewing Methods")
+            .font(.title2)
+            .padding(.top)
+            .multilineTextAlignment(.center)
+        /*https://icons8.com/icons/authors/RsIdElNdRw1Z/chattapat-./external-filled-outline-02-chattapat-/external-coffee-shop-filled-outline-02-chattapat-*/
+        VStack{
+            HStack{
+                Button(action: {
+                    print("Chemex")
+                }) {
+                    Image("chemex")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 65, height: 60)
+                        .clipped()
+                        //.cornerRadius(50)
+                }
+                .padding()
+                
+                /*VStack {
+                    Text("Chemex")
+                        .font(.system(size: 18, weight:.medium, design: .default))
+                }*/
+                
+                Button(action: {
+                    print("Aeropress")
+                })
+                {
+                    Image("aeropress")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 65, height: 60)
+                        .clipped()
+                        //.cornerRadius(50)
+                    /*VStack {
+                        Text("AeroPress")
+                            .font(.system(size: 18, weight:.medium, design: .default))
+                    }*/
+                }
+                .padding()
+                
+                Button(action: {
+                    print("V60")
+                })
+                {
+                    Image("pourOver")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 65, height: 60)
+                        .clipped()
+                        //.cornerRadius(50)
+                    /*VStack {
+                        Text("AeroPress")
+                            .font(.system(size: 18, weight:.medium, design: .default))
+                    }*/
+                }
+                .padding()
                 
             }
-
+            HStack {
+                Button(action: {
+                    print("Moka Pot")
+                })
+                {
+                    Image("moka")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 65, height: 60)
+                        .clipped()
+                        //.cornerRadius(50)
+                    /*VStack {
+                        Text("AeroPress")
+                            .font(.system(size: 18, weight:.medium, design: .default))
+                    }*/
+                    
+                }
+                .padding()
+                
+                Button(action: {
+                    print("French Press")
+                })
+                {
+                    Image("frenchpress")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 65, height: 60)
+                        .clipped()
+                        //.cornerRadius(50)
+                    /*VStack {
+                        Text("AeroPress")
+                            .font(.system(size: 18, weight:.medium, design: .default))
+                    }*/
+                }
+                .padding()
+                .accessibilityLabel(/*@START_MENU_TOKEN@*/"French Press"/*@END_MENU_TOKEN@*/)
+                
+                Button(action: {
+                    print("Cold Brew")
+                })
+                {
+                    Image("coldBrew")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 65, height: 60)
+                        .clipped()
+                        //.cornerRadius(50)
+                    /*VStack {
+                        Text("AeroPress")
+                            .font(.system(size: 18, weight:.medium, design: .default))
+                    }*/
+                }
+                .padding()
+            }
         }
+        
     }
 }
 
@@ -27,3 +132,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
--- Brewster/Brewster/DefaultBrew.swift
+++ Brewster/Brewster/DefaultBrew.swift
@@ -1,9 +1,10 @@
 //
-//  File.swift
+//  DefaultBrew.swift
 //  Brewster
 //
 //  Created by Josiah Murray on 09/05/2022.
 //
+
 import SwiftUI
 import Foundation
 
@@ -12,19 +13,19 @@ struct BrewingMethods: Identifiable {
     let name: String
     let image: String
     let description: String
-    let brewingTime: Float
     //let equipment: [{}]
     
 }
 
-struct BrewingInstructionss: Identifiable {
+struct BrewingInstructions: Identifiable {
     let id = UUID()
     let instructions: String
     let image: String
 }
 
 let BrewingMethodsList = [
-    BrewingMethods(name: "Chemex", image:"chemex_img", description: "A drip coffee brewing method perfect for coffee lovers who prefer a richer tasting coffee with little to no post brewing mess. ", brewingTime: 03.30)
+    BrewingMethods(name: "Chemex", image:"chemex", description: "A drip coffee brewing method perfect for coffee lovers who prefer a richer tasting coffee with little to no post brewing mess.")
     //BrewingMethods(name: "Hario V60", image:"V60_img", description: "A pour over coffee brewing method perfect for introducing coffee lovers to speciality brewing, and for people who prefer a richer tasting coffee with little to no preperation time. ", brewingTime: 02.30)
     //BrewingMethods(name: "AeroPress", image:"AeroPress_img", description: "A pressurised coffee brewing method perfect for quick early morning coffee lovers to speciality brewing, and for people who prefer a richer tasting coffee with little to no preperation time. ", brewingTime: 02.30)]
 ]
+
